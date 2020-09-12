74e3b0a3b7d2a5c8d44279172353bb3f86b362eb
cat3582 <cstbwy@gmail.com>
2020-09-13 00:09:27 +0900

    test_commit

---
+++ Swift_portfolio/Controller/AuthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthViewController.swift
+//  Swift_portfolio
+//
+//  Created by Yu Watanabe on 2020/09/12.
+//  Copyright © 2020 yu_w. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
