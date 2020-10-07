8a192e1b361b0618d7d8675989c80f8598feda76
cat3582 <cstbwy@gmail.com>
2020-10-07 23:43:17 +0900

    view_add NavigationController_deleted

---
+++ Swift_portfolio/Controller/MyTimeLineViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyTimeLineViewController.swift
+//  Swift_portfolio
+//
+//  Created by Yu Watanabe on 2020/10/07.
+//  Copyright © 2020 yu_w. All rights reserved.
+//
+
+import UIKit
+
+class MyTimeLineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
