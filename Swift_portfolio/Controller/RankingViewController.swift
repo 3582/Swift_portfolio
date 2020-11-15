de755aa5ce9da9aaaf3d4c6310bdecd32efb528c
cat3582 <cstbwy@gmail.com>
2020-11-15 13:18:06 +0900

    tabbar

---
+++ Swift_portfolio/Controller/RankingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RankingViewController.swift
+//  Swift_portfolio
+//
+//  Created by Yu Watanabe on 2020/11/15.
+//  Copyright © 2020 yu_w. All rights reserved.
+//
+
+import UIKit
+
+class RankingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
